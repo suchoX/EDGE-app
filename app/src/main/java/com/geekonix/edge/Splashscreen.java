75050e7926a13f7e22528b700d701eff7e058a6e
suchox <suchandrim.sarkar@gmail.com>
2016-03-08 15:28:59 +0530

    Initial commit

---
+++ app/src/main/java/com/geekonix/edge/Splashscreen.java
@@ -0,0 +1,70 @@
+package com.geekonix.edge;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.VideoView;
+
+import io.saeid.fabloading.LoadingView;
+
+public class Splashscreen extends AppCompatActivity{
+
+    DisplayMetrics metrics;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+        View decorView=getWindow().getDecorView();
+        int uiOptions= View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+
+        metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+
+        int height = metrics.heightPixels;
+        int width = metrics.widthPixels;
+
+        VideoView videoHolder = (VideoView) findViewById(R.id.videoView);
+        videoHolder.setLayoutParams(new RelativeLayout.LayoutParams(width,height));
+        try {
+            Uri video = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.edgex);
+            videoHolder.setVideoURI(video);
+            videoHolder.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mediaPlayer) {
+                    jump();
+                }
+            });
+            videoHolder.setZOrderOnTop(true);
+            videoHolder.start();
+        }
+        catch(Exception e) {
+            Log.e("abc", "Exception caught!", e);
+            jump();
+        }
+    }
+
+    private void jump()
+    {
+        //Jump to main activity
+        if(isFinishing())
+            return;
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+    }
+}
