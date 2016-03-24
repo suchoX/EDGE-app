569e2168f68e440cc31dfcbcceb1e0f16a2f7736
suchox <suchandrim.sarkar@gmail.com>
2016-03-25 01:18:27 +0530

    FunEvents added, Minor Changes

---
+++ app/src/main/java/com/geekonix/edge/FunEventsActivity.java
@@ -0,0 +1,13 @@
+package com.geekonix.edge;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FunEventsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fun_events);
+    }
+}
